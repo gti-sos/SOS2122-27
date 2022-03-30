f7ce3b465b0e5768814959b79dcf5b68b18d495c
jfeliperamos <jfeliperamos88@gmail.com>
2022-03-30 09:53:12 +0200

    Modificaciones en Index y trasbase de operaciones

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const path = require("path");
 
 const backend_roque = require("./src/back/public_expenditure_stats/index");
 const backend_alexis = require("./src/back/smi_stats/index");
+const backend_jf = require("./src/back/public_debt_stats/index");
 
 const app = express();
 
@@ -10,47 +11,21 @@ const app = express();
 var Datastore = require("nedb");
 var PE_DB = new Datastore({filename: path.join(__dirname,"./src/back/public_expenditure_stats/publicExpenditureDB.db"), autoload: true});
 var SMI_DB = new Datastore({filename: path.join(__dirname,"./src/back/smi_stats/smiDB.db"), autoload: true});
+var PD_DB = new Datastore({filename: path.join(__dirname,"./src/back/public_debt_stats/publicDebtDB.db"), autoload: true});
 
 const port = process.env.PORT || 8080;
-
 const bodyParser = require("body-parser");
-
-
-const JF_API = "/api/v1";
 
 app.use(bodyParser.json());
 
 backend_roque(app,PE_DB);
 backend_alexis(app, SMI_DB);
-
-var DebtStat = [
-    {
-        country: "espana",
-        year: 2020,
-        total_debt: 1345784,
-        debt_gdp: 120,
-        per_capita_debt: 28393
-
-    },   
-];
+backend_jf(app,PD_DB);
 
 app.use("/", express.static('public'))
 
-
 app.listen(port, () => {
     console.log(`Server TRULY ready`);
-});
-
-
-// OPERACIONES JF
-
-app.get(JF_API + "/public-debt-stats", (req,res)=>{
-    res.send(JSON.stringify(DebtStat, null, 2));
-});
-
-app.post(JF_API + "/public-debt-stats", (req,res)=>{
-    DebtStat.push(req.body);
-    res.sendStatus(201, "CREATED");
 });
 
 console.log(`Server ready at port ${port}`);
