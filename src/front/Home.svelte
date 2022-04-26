bc04b21c7647727568052124c4113ec7f2e0ebc0
jfeliperamos <jfeliperamos88@gmail.com>
2022-04-26 19:43:56 +0200

    Update /info y /home

--- src/front/Home.svelte
+++ src/front/Home.svelte
@@ -11,20 +11,20 @@
 <li><strong>URL</strong>: <a href="http://sos2122-27.herokuapp.com">http://sos2122-27.herokuapp.com</a></li>
 <li><strong>APIs V1</strong>:
 <ul>
-<li><a href="https://sos2122-27.herokuapp.com/api/v1/public-expenditure-stats">https://sos2122-27.herokuapp.com/api/v1/public-expenditure-stats</a> (developed by <a href="https://github.com/roque-fernandez">Roque Fernandez Iglesias</a>)</li>
-<li><a href="https://sos2122-27.herokuapp.com/api/v1/smi_stats">https://sos2122-27.herokuapp.com/api/v1/smi_stats</a> (developed by <a href="https://github.com/famvcucu">Fco Alexis Morales Vallado</a>)</li>
-<li><a href="https://sos2122-27.herokuapp.com/api/v1/public-debt-stats">https://sos2122-27.herokuapp.com/api/v1/public-debt-stats</a> (developed by <a href="https://github.com/jfeliperamos">Jose Felipe Ramos</a>)</li>
+<li><a href="https://sos2122-27.herokuapp.com/api/v1/public-expenditure-stats">/api/v1/public-expenditure-stats</a> (developed by <a href="https://github.com/roque-fernandez">Roque Fernandez Iglesias</a>)</li>
+<li><a href="https://sos2122-27.herokuapp.com/api/v1/smi_stats">/api/v1/smi_stats</a> (developed by <a href="https://github.com/famvcucu">Fco Alexis Morales Vallado</a>)</li>
+<li><a href="https://sos2122-27.herokuapp.com/api/v1/public-debt-stats">/api/v1/public-debt-stats</a> (developed by <a href="https://github.com/jfeliperamos">Jose Felipe Ramos</a>)</li>
 </ul>
 <li><strong>APIs V2</strong>:
 <ul>
-<li><a href="https://sos2122-27.herokuapp.com/api/v2/public-expenditure-stats">https://sos2122-27.herokuapp.com/api/v2/public-expenditure-stats</a> (developed by <a href="https://github.com/roque-fernandez">Roque Fernandez Iglesias</a>)</li>
-<li><a href="https://sos2122-27.herokuapp.com/api/v2/smi_stats">https://sos2122-27.herokuapp.com/api/v2/smi_stats</a> (developed by <a href="https://github.com/famvcucu">Fco Alexis Morales Vallado</a>)</li>
-<li><a href="https://sos2122-27.herokuapp.com/api/v2/public-debt-stats">https://sos2122-27.herokuapp.com/api/v2/public-debt-stats</a> (developed by <a href="https://github.com/jfeliperamos">Jose Felipe Ramos</a>)</li>
+<li><a href="https://sos2122-27.herokuapp.com/api/v2/public-expenditure-stats">/api/v2/public-expenditure-stats</a> (developed by <a href="https://github.com/roque-fernandez">Roque Fernandez Iglesias</a>)</li>
+<li><a href="https://sos2122-27.herokuapp.com/api/v2/smi_stats">/api/v2/smi_stats</a> (developed by <a href="https://github.com/famvcucu">Fco Alexis Morales Vallado</a>)</li>
+<li><a href="https://sos2122-27.herokuapp.com/api/v2/public-debt-stats">/api/v2/public-debt-stats</a> (developed by <a href="https://github.com/jfeliperamos">Jose Felipe Ramos</a>)</li>
 </ul>
 <li><strong>Frontends</strong>:<ul>
-<li><a href="https://sos2122-27.herokuapp.com/#/public-expenditure-stats">https://sos2122-27.herokuapp.com/#/public-expenditure-stats</a> (developed by <a href="https://github.com/roque-fernandez">Roque Fernandez Iglesias</a>)</li>
-<li><a href="https://sos2122-27.herokuapp.com/#/smi_stats">https://sos2122-27.herokuapp.com/#/smi_stats</a> (developed by <a href="https://github.com/famvcucu">Fco Alexis Morales Vallado</a>)</li>
-<li><a href="https://sos2122-27.herokuapp.com/#/public-debt-stats">https://sos2122-27.herokuapp.com/#/public-debt-stats</a> (developed by <a href="https://github.com/jfeliperamos">Jose Felipe Ramos</a>)</li>
+<li><a href="https://sos2122-27.herokuapp.com/#/public-expenditure-stats">/#/public-expenditure-stats</a> (developed by <a href="https://github.com/roque-fernandez">Roque Fernandez Iglesias</a>)</li>
+<li><a href="https://sos2122-27.herokuapp.com/#/smi_stats">/#/smi_stats</a> (developed by <a href="https://github.com/famvcucu">Fco Alexis Morales Vallado</a>)</li>
+<li><a href="https://sos2122-27.herokuapp.com/#/public-debt-stats">/#/public-debt-stats</a> (developed by <a href="https://github.com/jfeliperamos">Jose Felipe Ramos</a>)</li>
 </ul>
 </li>
 </ul>
--- src/front/Info.svelte
+++ src/front/Info.svelte
@@ -11,27 +11,27 @@
 <li><strong>URL</strong>: <a href="http://sos2122-27.herokuapp.com">http://sos2122-27.herokuapp.com</a></li>
 <li><strong>APIs V1</strong>:
 <ul>
-<li><a href="https://sos2122-27.herokuapp.com/api/v1/public-expenditure-stats">https://sos2122-27.herokuapp.com/api/v1/public-expenditure-stats</a> (developed by <a href="https://github.com/roque-fernandez">Roque Fernandez Iglesias</a>)</li>
-<li><a href="https://sos2122-27.herokuapp.com/api/v1/smi_stats">https://sos2122-27.herokuapp.com/api/v1/smi_stats</a> (developed by <a href="https://github.com/famvcucu">Fco Alexis Morales Vallado</a>)</li>
+<li><a href="https://sos2122-27.herokuapp.com/api/v1/public-expenditure-stats">/api/v1/public-expenditure-stats</a> (developed by <a href="https://github.com/roque-fernandez">Roque Fernandez Iglesias</a>)</li>
+<li><a href="https://sos2122-27.herokuapp.com/api/v1/smi_stats">/api/v1/smi_stats</a> (developed by <a href="https://github.com/famvcucu">Fco Alexis Morales Vallado</a>)</li>
 <li><a href="https://sos2122-27.herokuapp.com/api/v1/public-debt-stats">/api/v1/public-debt-stats</a> (developed by <a href="https://github.com/jfeliperamos">Jose Felipe Ramos</a>)</li>
 </ul>
 <li><strong>APIs V2</strong>:
 <ul>
-<li><a href="https://sos2122-27.herokuapp.com/api/v2/public-expenditure-stats">https://sos2122-27.herokuapp.com/api/v2/public-expenditure-stats</a> (developed by <a href="https://github.com/roque-fernandez">Roque Fernandez Iglesias</a>)</li>
-<li><a href="https://sos2122-27.herokuapp.com/api/v2/smi_stats">https://sos2122-27.herokuapp.com/api/v2/smi_stats</a> (developed by <a href="https://github.com/famvcucu">Fco Alexis Morales Vallado</a>)</li>
+<li><a href="https://sos2122-27.herokuapp.com/api/v2/public-expenditure-stats">/api/v2/public-expenditure-stats</a> (developed by <a href="https://github.com/roque-fernandez">Roque Fernandez Iglesias</a>)</li>
+<li><a href="https://sos2122-27.herokuapp.com/api/v2/smi_stats">/api/v2/smi_stats</a> (developed by <a href="https://github.com/famvcucu">Fco Alexis Morales Vallado</a>)</li>
 <li><a href="https://sos2122-27.herokuapp.com/api/v2/public-debt-stats">/api/v2/public-debt-stats</a> (developed by <a href="https://github.com/jfeliperamos">Jose Felipe Ramos</a>)</li>
 </ul>
 <li><strong>Frontends</strong>:<ul>
-<li><a href="https://sos2122-27.herokuapp.com/#/public-expenditure-stats">https://sos2122-27.herokuapp.com/#/public-expenditure-stats</a> (developed by <a href="https://github.com/roque-fernandez">Roque Fernandez Iglesias</a>)</li>
-<li><a href="https://sos2122-27.herokuapp.com/#/smi_stats">https://sos2122-27.herokuapp.com/#/smi_stats</a> (developed by <a href="https://github.com/famvcucu">Fco Alexis Morales Vallado</a>)</li>
+<li><a href="https://sos2122-27.herokuapp.com/#/public-expenditure-stats">/#/public-expenditure-stats</a> (developed by <a href="https://github.com/roque-fernandez">Roque Fernandez Iglesias</a>)</li>
+<li><a href="https://sos2122-27.herokuapp.com/#/smi_stats">/#/smi_stats</a> (developed by <a href="https://github.com/famvcucu">Fco Alexis Morales Vallado</a>)</li>
 <li><a href="https://sos2122-27.herokuapp.com/#/public-debt-stats">/#/public-debt-stats</a> (developed by <a href="https://github.com/jfeliperamos">Jose Felipe Ramos</a>)</li>
 </ul>
 <li><strong>Documentation</strong>:
 <ul>
-<li><a href="https://documenter.getpostman.com/view/8975262/UVyn2yyn">https://sos2122-27.herokuapp.com/api/v1/public-expenditure-stats</a> (developed by <a href="https://github.com/roque-fernandez">Roque Fernandez Iglesias</a>)</li>
-<li><a href="https://documenter.getpostman.com/view/8975262/Uyr8nyWh">https://sos2122-27.herokuapp.com/api/v2/public-expenditure-stats</a> (developed by <a href="https://github.com/roque-fernandez">Roque Fernandez Iglesias</a>)</li>
-<li><a href="https://documenter.getpostman.com/view/19481651/UVyn3K1F">https://sos2122-27.herokuapp.com/api/v1/smi_stats</a> (developed by <a href="https://github.com/famvcucu">Fco Alexis Morales Vallado</a>)</li>
-<li><a href="https://documenter.getpostman.com/view/19481651/UyrAGHTC">https://sos2122-27.herokuapp.com/api/v2/smi_stats</a> (developed by <a href="https://github.com/famvcucu">Fco Alexis Morales Vallado</a>)</li>
+<li><a href="https://documenter.getpostman.com/view/8975262/UVyn2yyn">public-expenditure-stats v1</a> (developed by <a href="https://github.com/roque-fernandez">Roque Fernandez Iglesias</a>)</li>
+<li><a href="https://documenter.getpostman.com/view/8975262/Uyr8nyWh">public-expenditure-stats v2</a> (developed by <a href="https://github.com/roque-fernandez">Roque Fernandez Iglesias</a>)</li>
+<li><a href="https://documenter.getpostman.com/view/19481651/UVyn3K1F">smi_stats v1</a> (developed by <a href="https://github.com/famvcucu">Fco Alexis Morales Vallado</a>)</li>
+<li><a href="https://documenter.getpostman.com/view/19481651/UyrAGHTC">smi_stats v2</a> (developed by <a href="https://github.com/famvcucu">Fco Alexis Morales Vallado</a>)</li>
 <li><a href="https://documenter.getpostman.com/view/14853996/UVypzxct">public-debt-stats v1</a> (developed by <a href="https://github.com/jfeliperamos">Jose Felipe Ramos</a>)</li>
 <li><a href="https://documenter.getpostman.com/view/14853996/UyrAGHgT">public-debt-stats v2</a> (developed by <a href="https://github.com/jfeliperamos">Jose Felipe Ramos</a>)</li>
 </ul>
