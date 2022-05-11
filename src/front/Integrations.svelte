78ede21768d035192d305782bac9449733e8f764
roque-fernandez <roque.f.i7@gmail.com>
2022-05-11 20:20:21 +0200

    3 primeras integraciones

--- src/front/Integrations.svelte
+++ src/front/Integrations.svelte
@@ -4,7 +4,7 @@
 <ul>
 <li><a href="/#/public-expenditure-stats/int-v1">Integración externa de la ciudad de Innsbruck</a> </li>
 <li><a href="/#/public-expenditure-stats/int-v2">Random number</a> </li>
-<li><a href="/#/public-expenditure-stats/int-v3">Random number</a> </li>
+<li><a href="/#/public-expenditure-stats/int-v3">Estadísticas de jugadores de la NBA</a> </li>
 
 </ul>
 </li>
